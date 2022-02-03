b1a26afc6ad28515c131f245a6ac0043c9650ab9
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-02-03 03:14:31 +0000

    admin dashboard created

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,5 +5,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name:"Jungle", password: "book"
 
   def show
+    @product_count = Product.count 
+
+    @category_list = Product.group(:category_id).count
   end
 end
